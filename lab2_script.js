c949bee72af8f7a4b887760ca00d760da0766315
Barkat28Singh <Barkat1213singh@gmail.com>
2022-10-06 21:49:17 -0400

    edited branch1

--- lab2_script.js
+++ lab2_script.js
@@ -1,4 +1,4 @@
-document.querySelector("header").style.color = "red";
+document.querySelector("header").style.color = "purple";
 document.querySelector("header").style.textAlign = "center";
 document.querySelector("header").style.border = "2px solid green";
 
